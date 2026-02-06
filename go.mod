50b7d13c8f1b14333cd5a0fdbd0bf84d1529e5cc
Dmitriy <d.altuhov1@gmail.com>
2026-02-06 20:18:07 +0300

    some fixs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module my-crypto
 
 go 1.25.3
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- internal/web/web.go
+++ internal/web/web.go
@@ -1,17 +1,28 @@
 package web
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
 
 type WsChatServer interface {
 	Test(w http.ResponseWriter, r *http.Request)
 }
 type wsServer struct {
+	wsUpgrader *websocket.Upgrader
 }
 
 func NewWsServer() (WsChatServer, error) {
-	return &wsServer{}, nil
+	return &wsServer{
+		wsUpgrader: &websocket.Upgrader{},
+	}, nil
 }
 
-func (h *wsServer) Test(w http.ResponseWriter, r *http.Request) {
+func (ws *wsServer) Test(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello Guys"))
+}
+
+func (ws *wsServer) TcpHandShakeForWs(w http.ResponseWriter, r *http.Request) {
+	con, err := ws.wsUpgrader.Upgrade(w, r, nil)
 }
